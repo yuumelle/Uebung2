9486242d7ed11ffecd897e4fe532ba330ebdc906
yuumelle <mle9@smail.uni-koeln.de>
2025-10-28 19:25:36 +0100

    P1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,17 @@
+package p1;
+
+import java.util.Scanner;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
+
+	}
+
+}
