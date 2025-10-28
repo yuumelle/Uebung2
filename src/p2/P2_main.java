f1e86a962dd2f1f10556fbb80b0793400e5c324d
yuumelle <mle9@smail.uni-koeln.de>
2025-10-28 19:38:27 +0100

    P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		boolean b = true;
+		if (i > 10) {
+			b = true;
+		}
+		System.out.println("i hat den Wert: " + i + ", b hat den Wert:" + b);
+	}
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,18 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int j = 2;
+		int k = 3;
+		if (i < j)
+			System.out.println("A");
+		if (i < k)
+			System.out.println("B");
+		if (j < k)
+			System.out.println("C");
+
+	}
+}
